f190e453bfdba8a892b9b5f0faddeefa1db46b90
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2024-01-19 13:11:09 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+'use client'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [billAmount, setBillAmount] = useState(0)
+  const [tipPercentage, setTipPercentage] = useState(0)
+  const [numberOfPeople, setNumberOfPeople] = useState(0)
+  const [tipAmount, setTipAmount] = useState(0)
+  const [total, setTotal] = useState(0)
+  console.log(billAmount)
+  console.log(tipAmount)
+
+  const resetAll = () => {
+    setBillAmount(0)
+    setTipPercentage(0)
+    setNumberOfPeople(0)
+    setTipAmount(0)
+    setTotal(0)
+  }
+
   return (
     <main className='bg-[#c5e4e7] flex flex-col md:justify-center items-center md:min-h-screen'>
 
@@ -18,30 +37,30 @@ export default function Home() {
             <p className='font-mono font-semibold text-gray-700'>Bill</p>
             <div className='flex bg-slate-200 gap-1 justify-between items-center px-4 rounded-lg'>
               <Image src='/images/icon-dollar.svg' alt='person' width={5} height={5} className='text-white w-3 h-5'/>
-              <input type="text" className='bg-inherit w-full h-10 outline-none text-end text-[#00494d] font-semibold font-mono text-xl'/>
+              <input type="text" className='bg-inherit w-full h-10 outline-none text-end text-[#00494d] font-semibold font-mono text-xl' value={billAmount} onChange={(e) => setBillAmount(e.target.value)}/>
             </div>
           </div>
 
           <div className='flex flex-col gap-2'>
             <p className='font-mono font-semibold'>Select Tip %</p>
             <div className='grid grid-cols-2 md:grid-cols-3 gap-4'>
-              <div className='bg-[#00494d] text-center py-2 rounded-md text-white font-mono text-xl font-semibold cursor-pointer hover:bg-[#26c0ab] hover:text-gray-800'>
-                <p>5%</p>
+              <div className='bg-[#00494d] text-center py-2 rounded-md text-white font-mono text-xl font-semibold cursor-pointer hover:bg-[#26c0ab] hover:text-gray-800 z-0'>
+                <p onChange={(e) => setTipPercentage(e.target.value)} className='z-10'>5%</p>
               </div>
               <div className='bg-[#00494d] text-center py-2 rounded-md text-white font-mono text-xl font-semibold cursor-pointer hover:bg-[#26c0ab] hover:text-gray-800'>
-                <p>10%</p>
+                <p onChange={(e) => setTipPercentage(e.target.value)}>10%</p>
               </div>
               <div className='bg-[#00494d] text-center py-2 rounded-md text-white font-mono text-xl font-semibold cursor-pointer hover:bg-[#26c0ab] hover:text-gray-800'>
-                <p>15%</p>
+                <p onChange={(e) => setTipPercentage(e.target.value)}>15%</p>
               </div>
               <div className='bg-[#00494d] text-center py-2 rounded-md text-white font-mono text-xl font-semibold cursor-pointer hover:bg-[#26c0ab] hover:text-gray-800'>
-                <p>25%</p>
+                <p onChange={(e) => setTipPercentage(e.target.value)}>25%</p>
               </div>
               <div className='bg-[#00494d] text-center py-2 rounded-md text-white font-mono text-xl font-semibold cursor-pointer hover:bg-[#26c0ab] hover:text-gray-800'>
-                <p>50%</p>
+                <p onChange={(e) => setTipPercentage(e.target.value)}>50%</p>
               </div>
               <div>
-                <input type="text" placeholder='custom' className='bg-slate-200 w-full h-10 outline-none text-end text-[#00494d] font-semibold font-mono text-xl rounded-lg px-3 h-full'/>
+                <input type="text" placeholder='custom' className='bg-slate-200 w-full outline-none text-end text-[#00494d] font-semibold font-mono text-xl rounded-lg px-3 h-full' value={tipPercentage} onChange={(e) => setTipPercentage(e.target.value)}/>
               </div>
             </div>
           </div>
@@ -49,7 +68,7 @@ export default function Home() {
             <p className='font-mono font-semibold text-gray-700'>Number of People</p>
             <div className='flex bg-slate-200 gap-1 justify-between items-center px-4 rounded-lg'>
               <Image src='/images/icon-person.svg' alt='person' width={10} height={10} className='text-white w-4 h-4'/>
-              <input type="text" className='bg-inherit w-full h-10 outline-none text-end text-[#00494d] font-semibold font-mono text-xl'/>
+              <input type="text" className='bg-inherit w-full h-10 outline-none text-end text-[#00494d] font-semibold font-mono text-xl' value={numberOfPeople} onChange={(e) => setNumberOfPeople(e.target.value)}/>
             </div>
           </div>
         </div>
@@ -62,17 +81,17 @@ export default function Home() {
                 <p className='text-sm'>Tip Amount</p>
                 <p className='text-xs text-slate-200'>/ person</p>
               </div>
-              <p className='text-4xl font-mono font-semibold text-[#26c0ab]'>$4.27</p>
+              <p className='text-4xl font-mono font-semibold text-[#26c0ab]'>{`$${tipAmount}`}</p>
             </div>
             <div className='flex justify-between gap-2 items-center'>
               <div>
                 <p className='text-sm'>Total</p>
                 <p className='text-xs text-slate-200'>/ person</p>
               </div>
-              <p className='text-4xl font-mono font-semibold text-[#26c0ab]'>$32.79</p>
+              <p className='text-4xl font-mono font-semibold text-[#26c0ab]'>{`$${total}`}</p>
             </div>
           </div>
-          <div className='bg-[#26c0ab] rounded py-2 text-center text-gray-900 font-semibold uppercase cursor-pointer'>
+          <div className='bg-[#26c0ab] rounded py-2 text-center text-gray-900 font-semibold uppercase cursor-pointer' onClick={resetAll}>
             Reset
           </div>
         </div>
